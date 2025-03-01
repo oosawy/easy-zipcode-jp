fc520c60c98378489834adacc4cf22f68c55b58b
Yuta Osawa <yutaosawa81@gmail.com>
2025-03-01 19:01:39 +0900

    First Landing 🛬 (#2)
    * First Landing
    
    * Fix package dependency specifier
    
    * Run publint on check script
    
    * Run publint after build
    
    * Add missing build step
    
    * test: check strictly
    
    * test
    
    * test
    
    * fix
    
    * Add README.md

---
+++ src/internal.ts
@@ -0,0 +1,48 @@
+export type Address = {
+  zip: string
+  prefectures: string
+  city: string
+  other: string
+}
+
+export const getAreaMap = async (areaCode: string) => {
+  try {
+    const module = await import(`../assets/${areaCode}.json`)
+    return module.default
+  } catch {
+    return null
+  }
+}
+
+export const lookup = async (input: string): Promise<Address[] | null> => {
+  const areaCode = parseAreaCode(input).area
+  return await getAreaMap(areaCode)
+}
+
+export const resolve = async (input: string): Promise<Address | null> => {
+  const zipCode = parseZipCode(input).zip
+
+  const areaCode = zipCode.slice(0, 3)
+  const area = await lookup(areaCode)
+
+  return area?.find((address) => address.zip === zipCode) ?? null
+}
+
+export const parseAreaCode = (input: string) => {
+  const match = input.match(/^(\d{3})$/)?.[0]
+
+  if (match) return { area: match }
+
+  throw new Error('Invalid Area code: It must follow the pattern: 100.')
+}
+
+export const parseZipCode = (input: string) => {
+  const matches = input.match(/^(\d{3})-?(\d{4})$/)
+
+  const [, area, local] = matches ?? []
+  if (area && local) return { area, local, zip: area + local }
+
+  throw new Error(
+    'Invalid ZIP code: It must follow the pattern: 100-0001 or 1000001.'
+  )
+}
